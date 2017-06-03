45c112a908e0bf2d356a0da7b3b7d9c975233384
Bass Jansson <bassjansson@gmail.com>
2017-06-03 14:37:41 +0200

    Adds custom Menlo fonts.

--- scripts/home.js
+++ scripts/home.js
@@ -1,37 +1,44 @@
-function homeAnimation()
+
+function animate()
 {
-  var canvas = document.getElementById("mainCanvas");
-  var ctx = canvas.getContext("2d");
-  var pos = 0;
-  var id = setInterval(frame, 120);
-  var myText = "Hello, my name is Bass.";
-  var posX = 100;
-  var posY = 300;
+    var canvas = document.getElementById("mainCanvas");
+    var ctx = canvas.getContext("2d");
+    var pos = 0;
+    var id = setInterval(frame, 120);
+    var myText = "Hello, my name is Bass.";
+    var posX = 100;
+    var posY = 300;
 
-  ctx.font = "bold 128px Menlo";
-  ctx.textAlign = "left";
+    ctx.font = "128px Menlo Bold";
+    ctx.textAlign = "left";
 
-  ctx.fillStyle = "rgb(200, 200, 200)";
-  ctx.fillText(">", posX, posY);
+    ctx.fillStyle = "rgb(200, 200, 200)";
+    ctx.fillText(">", posX, posY);
 
-  function frame()
-  {
-    if (pos >= myText.length)
+    function frame()
     {
-      ctx.fillStyle = "rgb(200, 200, 200)";
-      ctx.fillText(">", posX, posY + 128);
-      clearInterval(id);
-    }
-    else
-    {
-      if (Math.random() > 0.5)
-      {
-          //ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.fillStyle = "rgb(90, 20, 150)";
-          ctx.fillText(myText.slice(pos, pos + 1), posX + 32 + 76 * (pos + 1), posY);
+        if (pos >= myText.length)
+        {
+            ctx.fillStyle = "rgb(200, 200, 200)";
+            ctx.fillText(">", posX, posY + 128);
+            clearInterval(id);
+        }
+        else
+        {
+            if (Math.random() > 0.5)
+            {
+                //ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.fillStyle = "rgb(90, 20, 150)";
+                ctx.fillText(myText.slice(pos, pos + 1), posX + 32 + 76 * (pos + 1), posY);
 
-          pos++;
-      }
+                pos++;
+            }
+        }
     }
-  }
+}
+
+function animateHome()
+{
+    // Wait for our font to load
+    setTimeout(function(){ animate(); }, 100);
 }
